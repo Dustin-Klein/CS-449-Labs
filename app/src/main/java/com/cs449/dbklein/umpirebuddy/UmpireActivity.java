bd788b6c5cc4db98c49dfefbe2935b5587c8ac7e
Dustin Klein <dbklein4@gmail.com>
2018-08-31 23:24:04 -0500

    Added alerts for walks and strikeouts

--- app/src/main/java/com/cs449/dbklein/umpirebuddy/UmpireActivity.java
+++ app/src/main/java/com/cs449/dbklein/umpirebuddy/UmpireActivity.java
@@ -1,7 +1,9 @@
 package com.cs449.dbklein.umpirebuddy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -25,21 +27,48 @@ public class UmpireActivity extends AppCompatActivity {
         ballsText = findViewById(R.id.balls_text);
         strikesText = findViewById(R.id.strikes_text);
 
-        updateCountText(balls, strikes);
-
         Button ballsButton = findViewById(R.id.balls_button);
         Button strikesButton = findViewById(R.id.strikes_button);
+
+        final AlertDialog.Builder walkAlert = new AlertDialog.Builder(UmpireActivity.this)
+                .setMessage("Take your base.")
+                .setCancelable(true)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+        final AlertDialog.Builder strikeOutAlert = new AlertDialog.Builder(UmpireActivity.this)
+                .setMessage("You're Out!")
+                .setCancelable(true)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+        updateCountText(balls, strikes);
+
 
         ballsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Every click increment the ball counter
                 ++balls;
 
+                // If it is the fourth ball, reset the count and show an alert
                 if (balls == 4) {
                     balls = 0;
                     strikes = 0;
+
+                    walkAlert.show();
                 }
 
+                // Display the changes
                 updateCountText(balls, strikes);
             }
         });
@@ -47,13 +76,18 @@ public class UmpireActivity extends AppCompatActivity {
         strikesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Every click increment the strike counter
                 ++strikes;
 
+                // If it is the third strike, reset the count and show an alert
                 if (strikes == 3) {
                     balls = 0;
                     strikes = 0;
+
+                    strikeOutAlert.show();
                 }
 
+                // Display the changes
                 updateCountText(balls, strikes);
             }
         });
